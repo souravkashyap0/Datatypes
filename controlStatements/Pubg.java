a1c8236116bbc1a845ee25d7efefd0e1151afb3a
Sourav N <55662036+souravkashyap0@users.noreply.github.com>
2021-09-07 22:58:56 +0530

    Control statements IF

---
+++ controlStatements/Pubg.java
@@ -0,0 +1,12 @@
+class Pubg
+{
+	public static void main(String [] args)
+	{
+		int ram=4;
+		if(ram>=6)
+		{
+			System.out.println("Pubg installed successfully");
+			
+		}
+	}
+}
---
+++ controlStatements/TicketBooking.java
@@ -0,0 +1,12 @@
+class TicketBooking
+{
+	public static void main(String [] args)
+	{
+		int ticketAvailable=0;
+		if(ticketAvailable>=1)
+		{
+			System.out.println("Tickets are available and you can book them ");
+			
+		}
+	}
+}
